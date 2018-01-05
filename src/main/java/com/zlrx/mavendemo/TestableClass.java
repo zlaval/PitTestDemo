b089a8fbfd7570022fa09d23a278377eb29ffe86
Zalan <zlaval@gmail.com>
2018-01-05 20:45:29 +0100

    pitest demo

---
+++ src/main/java/com/zlrx/mavendemo/TestableClass.java
@@ -0,0 +1,18 @@
+package com.zlrx.mavendemo;
+
+public class TestableClass {
+
+    public int returnWithTwo() {
+        return 2;
+    }
+
+    public boolean isPositive(int number) {
+        if (number >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
